167804e26fa45b819e8257f44bba695b63c25d5d
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-03-17 02:13:52 +0000

    Estruturação do Catálogo

---
+++ src/main/java/Projeto01/controller/CatalogoVeiculos.java
@@ -0,0 +1,35 @@
+package src.main.java.Projeto01.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoVeiculos {
+    private List<Veiculo> veiculos;
+    
+    public CatalogoVeiculos() {
+        this.veiculos = new ArrayList<>();
+    }
+    
+
+
+    public Veiculo consultaPorPlaca(String placa){
+
+    }
+
+    public List<Veiculo> consultaPorMarca(String marca){
+
+    }
+
+    public List<Veiculo> consultaPorAno(int ano){
+
+    }
+
+    public List<Veiculo> consultaPorTipo(String tipo){
+        
+    }
+
+    
+    
+
+
+}
