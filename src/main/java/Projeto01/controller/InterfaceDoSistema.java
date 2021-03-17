6aec01047878b97f97ccc320861ccc22fb6132b0
joaogabrieldlb <joaogabrieldlb@hotmail.com>
2021-03-17 05:38:35 +0000

    Versão 3 - Interface do Sistema

--- src/main/java/Projeto01/controller/InterfaceDoSistema.java
+++ src/main/java/Projeto01/controller/InterfaceDoSistema.java
@@ -7,82 +7,81 @@ import src.main.java.Projeto01.model.Veiculo;
 
 public class InterfaceDoSistema {
     
-    private boolean loop;
-    private List<Veiculo> listaConsulta;
+    private List<Veiculo> listaDaConsulta;
     private CatalogoVeiculos cv;
+    private Scanner sc = new Scanner(System.in);
+    private String resposta;
 
     public InterfaceDoSistema() {
-        loop = true;
         cv = new CatalogoVeiculos();
+        
     }
 
     public void executa() {
-        while(loop == true) {
+        while(true) {
             System.out.println("Seja bem-vindo a interface do sistema de veículos!" + System.lineSeparator());
-            if(consultaVeiculo() == false) {
-                loop = false;
+            if(menuConsulta() == false) {
+                break;
             }
         }
     }
 
-    private boolean consultaVeiculo() {
-        while(loop) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("--= Menu de Consulta =--");
+    private boolean menuConsulta() {
+        while(true) {
+            System.out.println("--= Menu de Consulta =--" + System.lineSeparator());
+            System.out.println("Digite:");
             System.out.println("1. Consultar por placa");
             System.out.println("2. Consultar por marca");
-            System.out.println("3. Consultar por ano de fabrica");
-            System.out.println("4. Consultar por tipo");
-            System.out.print(": ");
-            String respostaNum = sc.nextLine();
+            System.out.println("3. Consultar por ano de \nfabricação");
+            System.out.println("4. Consultar por tipo" + System.lineSeparator());
+            System.out.println("------------------------");
+            System.out.println("0. Sair");
+            System.out.println("________________________");
+            System.out.print("-> ");
+            resposta = sc.nextLine();
             System.out.println();
             
-            if(respostaNum.equals("0")) { sc.close(); return false; }
+            if(resposta.equals("0")) return false;
 
-            if(respostaNum.equals("1") || respostaNum.equals("2") ||
-             respostaNum.equals("3") || respostaNum.equals("4")) {
-                if(respostaNum.equals("1")) System.out.print("Número da placa: ");
-                if(respostaNum.equals("2")) System.out.print("Marca do veículo: ");
-                if(respostaNum.equals("3")) System.out.print("Ano de Fabricação: ");
-                if(respostaNum.equals("4")) {
-                    System.out.println("1. Veículo Passeio");
-                    System.out.println("2. Veículo Passageiros");
-                    System.out.println("3. Veículo Utilitário");
-                    System.out.print("Tipo do veículo: ");
-                }
-               	String consultaInfo = sc.nextLine();
-                
-                if(respostaNum.equals("4") && consultaInfo.equals("1")) consultaInfo = "VeiculoPasseio";
-                if(respostaNum.equals("4") && consultaInfo.equals("2")) consultaInfo = "VeiculoPassageiros";
-                if(respostaNum.equals("4") && consultaInfo.equals("3")) consultaInfo = "VeiculoUtilitario";
-
-                System.out.println();
-                switch(respostaNum) {
-                    case "1":
-                    // TODO: if(cv.consultaPorPlaca(consultaInfo) == null) ;
-                    break;
-                    case "2":
-                    listaConsulta = cv.consultaPorMarca(consultaInfo);
-                    break;
-                    case "3":
-                    listaConsulta = cv.consultaPorAno(Integer.parseInt(consultaInfo));
-                    break;
-                    case "4":
-                    listaConsulta = cv.consultaPorTipo(consultaInfo);
-                }
-    
-                listaConsulta.forEach(veiculo -> {
-                    System.out.println(veiculo);
-                });
-                sc.close();
-                return true;
-            } else {
-                System.out.println("Erro: Foi digitado um número inválido!");
-            }
-            sc.close();
-            break;
+            if(resposta.equals("1") || resposta.equals("2") ||
+             resposta.equals("3") || resposta.equals("4")) consulta(resposta);
+            else System.out.println("Erro: Foi digitada uma opção inválida!" + System.lineSeparator() +
+            "Tente novamente..." + System.lineSeparator());
         }
-        return false;
+    }
+
+    private boolean consulta(String tipoDaConsulta) {
+        while(true) {
+            if(tipoDaConsulta.equals("1")){
+                if(consultaPorPlaca() == null) {
+                    System.out.println("Erro: Veículo não encontrado no sistema!" + System.lineSeparator() +
+                    "Tente novamente..." + System.lineSeparator());
+                    return false;
+                }
+                System.out.println(consultaPorPlaca());
+                return true;
+            }
+
+            //TODO: if(tipoDaConsulta.equals("2")) System.out.print("Marca do veículo: ");
+            
+            //TODO: if(tipoDaConsulta.equals("3")) System.out.print("Ano de Fabricação: ");
+            /* TODO: if(tipoDaConsulta.equals("4")) {
+                System.out.println("1. Veículo Passeio");
+                System.out.println("2. Veículo Passageiros");
+                System.out.println("3. Veículo Utilitário");
+                System.out.print("Tipo do veículo: ");
+            }
+            */
+
+            if(resposta.equals("0")) return false;
+        }
+    }
+
+    private Veiculo consultaPorPlaca() {
+        System.out.print("Número da placa: ");
+        resposta = sc.nextLine();
+        System.out.println();
+        return cv.consultaPorPlaca(resposta);
     }
 
 }
