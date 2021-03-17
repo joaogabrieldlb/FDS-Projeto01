77e703611afa8748eaca1193e9c816ae57294f7f
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-03-17 02:49:05 +0000

    Atualização classe CatalogoVeiculos

--- src/main/java/Projeto01/controller/CatalogoVeiculos.java
+++ src/main/java/Projeto01/controller/CatalogoVeiculos.java
@@ -8,24 +8,34 @@ public class CatalogoVeiculos {
     
     public CatalogoVeiculos() {
         this.veiculos = new ArrayList<>();
+        
     }
     
 
 
     public Veiculo consultaPorPlaca(String placa){
-
+        return veiculos.stream()
+            .filter(f -> f.getPlaca().equals(placa))
+            .findAny()
+            .orElse(null);
     }
 
     public List<Veiculo> consultaPorMarca(String marca){
-
+        return veiculos.stream()
+            .filter(f -> f.getMarca().equals(marca))
+            .collect();
     }
 
     public List<Veiculo> consultaPorAno(int ano){
-
+        return veiculos.stream()
+            .filter(f -> f.getAno() == ano)
+            .collect();
     }
 
     public List<Veiculo> consultaPorTipo(String tipo){
-        
+        return veiculos.stream()
+            .filter(f -> f.getClass().getSimpleName().equals(tipo))
+            .collect();
     }
 
     
